2ad3704f794cd38af051ec4ed70ad083113e9f46
theo <theo@zibell.se>
2022-09-05 16:18:01 +0200

    update text

--- ​Tamagotchi/Display.cs
+++ ​Tamagotchi/Display.cs
@@ -3,5 +3,20 @@ using System;
 
 public class Display
 {
+    public static void Line(string text)
+    {
+        System.Console.WriteLine("-------------------------------------");
+        System.Console.WriteLine($"|   {text}    |");
+        System.Console.WriteLine("-------------------------------------");
 
+        System.Console.WriteLine(text.Length);
+        Console.ReadLine();
+
+    }
+    public static void Start()
+    {
+        string text = "Välkomen till Tamagotchi";
+        Line(text);
+
+    }
 }
--- ​Tamagotchi/Program.cs
+++ ​Tamagotchi/Program.cs
@@ -4,4 +4,5 @@ Tamagotchi t = new Tamagotchi();
 
 t.Name = "Jeff";
 t.Feed();
+Display.Start();
 
